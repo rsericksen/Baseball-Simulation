7207c5e2ffcfedc28f8042bcd730e93f9e743ad2
rsericksen <rsericksen@gmail.com>
2014-10-23 01:27:15 -0500

    created probability functions, functional sim
    Baltimore always generates a ridiculously high score and the Yankees
    generate ridiculously low scores. Needs to be tweaked somehow.

--- Baseball.py
+++ Baseball.py
@@ -1,3 +1,11 @@
+from random import randrange
+
+def getDouble(single,double,batter):
+	return (single[batter]+double[batter])*100;
+
+def getTriple(single,double,triple,batter):
+	return (single[batter]+double[batter]+triple[batter])*100;
+
 nyyPlayerName = ["Ellsbury","Jeter","Beltran","Teixeira","McCann","Gardner","Johnson","Suzuki"]
 balPlayerName = ["Markakis","Flaherty","Davis","Jones","Cruz","Hardy","Lough","Weeks"];
 nyyAvg = [0.271,0.256,0.233,0.216,0.232,0.256,0.219,0.284];
@@ -11,17 +19,219 @@ balTriple = [0.005,0.017,0.000,0.011,0.013,0.000,0.069,0.333];
 nyyHomeRun = [0.103,0.027,0.160,0.232,0.200,0.120,0.136,0.010];
 balHomeRun = [0.079,0.112,0.295,0.160,0.240,0.063,0.093,0.000];
 
-print "New York Yankees Batting Averages";
+inning = 1;
+outs = 0;
+nyyScore = 0;
+balScore = 0;
+batter = 0;
+onBase = 0;
+x = 0;
 
-for i in range(0,8):
-	print nyyPlayerName[i];
-	print nyyAvg[i];
-	i+=1;
+firstBase = False;
+secondBase = False;
+thirdBase = False;
 
-print "\n";
-print "Baltimore Orioles Batting Averages";
+while (inning<=9 or nyyScore==balScore):
+	print "Top of Inning "+str(inning);
+	while (outs<3):
+		print nyyPlayerName[batter]+" is up at bat.";
+		x = randrange(0,100);
+		if(x<=nyyAvg[batter]*100):
+			x = randrange(0,100)
 
-for i in range(0,8):
-        print balPlayerName[i];
-        print balAvg[i];
-        i+=1;
+			if (x<nyySingle[batter]*100):
+				if thirdBase:
+					nyyScore += 1;
+					onBase -= 1;
+					thirdBase = False;
+					print "Yankees Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+				if secondBase:
+					thirdBase = True;
+					secondBase = False;
+
+				if firstBase:
+					secondBase = True;
+					firstBase = False;
+
+				onBase += 1;
+				firstBase = True;
+
+			elif ((x>=nyySingle[batter]*100) and (x<getDouble(nyySingle,nyyDouble,batter))):
+				if thirdBase:
+                                        nyyScore += 1;
+                                        onBase -= 1;
+                                        thirdBase = False;
+					print "Yankees Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                                if secondBase:
+                                        nyyScore +=1;
+					onBase -= 1;
+                                        secondBase = False;
+					print "Yankees Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                                if firstBase:
+                                        thirdBase = True;
+                                        firstBase = False;
+
+				onBase += 1;
+				secondBase = True;
+
+			elif ((x>=getDouble(nyySingle,nyyDouble,batter)) and (x<getTriple(nyySingle,nyyDouble,nyyTriple,batter))):
+				if thirdBase:
+                                        nyyScore += 1;
+                                        onBase -= 1;
+                                        thirdBase = False;
+					print "Yankees Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                                if secondBase:
+                                        nyyScore += 1;
+                                        onBase -= 1;
+					secondBase = False;
+					print "Yankees Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                                if firstBase:
+                                        nyyScore += 1;
+					onBase -= 1;
+                                        firstBase = False;
+					print "Yankees Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+				onBase += 1;
+				thirdBase = True;
+
+			elif ((x>=getTriple(nyySingle,nyyDouble,nyyTriple,batter)) and (x<=100)):
+				nyyScore += (onBase+1);
+				firstBase = False;
+				secondBase = False;
+				thirdBase = False;
+				onBase = 0;
+				print "Yankees Score!";
+				print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+	
+		else:
+			outs += 1;
+			print nyyPlayerName[batter]+" is out.";
+			print str(outs)+" outs.";
+
+		batter += 1;
+		if (batter == 8):
+			batter = 0;
+	
+	firstBase = False;
+        secondBase = False;
+        thirdBase = False;
+        onBase = 0;
+	batter = 0;
+	outs = 0;	
+
+	print "\n";
+	print "Bottom of Inning "+str(inning);
+	
+	while (outs<3):
+		print balPlayerName[batter]+" is up at bat.";
+		x = randrange(0,100);
+                if(x<=balAvg[batter]*100):
+                        x = randrange(0,100)
+
+                        if (x<balSingle[batter]*100):
+                                if thirdBase:
+                                        balScore += 1;
+                                        onBase -= 1;
+                                        thirdBase = False;
+					print "Orioles Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                                if secondBase:
+                                        thirdBase = True;
+                                        secondBase = False;
+
+                                if firstBase:
+                                        secondBase = True;
+                                        firstBase = False;
+
+                                onBase += 1;
+                                firstBase = True;
+
+		elif ((x>=balSingle[batter]*100) and (x<getDouble(balSingle,balDouble,batter))):
+                                if thirdBase:
+                                        balScore += 1;
+                                        onBase -= 1;
+                                        thirdBase = False;
+					print "Orioles Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                                if secondBase:
+                                        balScore +=1;
+                                        onBase -= 1;
+                                        secondBase = False;
+					print "Orioles Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                                if firstBase:
+                                        thirdBase = True;
+                                        firstBase = False;
+
+                                onBase += 1;
+                                secondBase = True;
+
+		elif ((x>=getDouble(balSingle,balDouble,batter)) and (x<getTriple(balSingle,balDouble,balTriple,batter))):
+                                if thirdBase:
+                                        balScore += 1;
+                                        onBase -= 1;
+                                        thirdBase = False;
+					print "Orioles Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                                if secondBase:
+                                        balScore += 1;
+                                        onBase -= 1;
+                                        secondBase = False;
+					print "Orioles Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                                if firstBase:
+                                        balScore += 1;
+                                        onBase -= 1;
+                                        firstBase = False;
+					print "Orioles Score!";
+					print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                                onBase += 1;
+                                thirdBase = True;
+
+		elif ((x>=getTriple(balSingle,balDouble,balTriple,batter)) and (x<=100)):
+                                balScore += (onBase+1);
+                                firstBase = False;
+                                secondBase = False;
+                                thirdBase = False;
+                                onBase = 0;
+				print "Orioles Score!";
+				print "Yankees: "+str(nyyScore)+"     "+"Orioles: "+str(balScore);
+
+                else:
+                        outs += 1;
+			print balPlayerName[batter]+" is out.";
+			print str(outs)+" outs.";
+
+		batter += 1;
+                if (batter == 8):
+                        batter = 0;
+	
+	inning += 1;
+	firstBase = False;
+        secondBase = False;
+        thirdBase = False;
+        onBase = 0;
+	batter = 0;
+	outs = 0;
+
+	print "\n";
+
+print "After "+str(inning-1)+" Innings";
+print "Yankees: "+str(nyyScore)+"     "+ "Orioles: "+str(balScore);
